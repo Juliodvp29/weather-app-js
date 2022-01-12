07b23de4e547ff08bc3303a85c6600095f4d6184
Julio enrique <75991305+Juliodvp29@users.noreply.github.com>
2022-01-12 11:44:40 -0500

    Update weather.js

--- weather-app-js/JS/weather.js
+++ weather-app-js/JS/weather.js
@@ -1,74 +1,132 @@
 const d = document;
 
 const apikey = "bcae56b33c6e9fbf69fade1501c12372";
+let _weather = {};
 
-let ciudad = document.getElementById('name-city');
-let temp = document.getElementById('info-temp');
-let time = document.getElementById('info-time');
-let nubosidad = document.getElementById('info-nubosidad');
-let humedad = document.getElementById('info-humedad');
-let viento = document.getElementById('info-viento');
-let atardecer = document.getElementById('info-atardecer');
-let psol = document.getElementById('info-psol');
-let ltd = document.getElementById('info-ltd');
-let desc = document.getElementById('info-desc');
+const btn = d.getElementById("btn-find");
+let city = d.getElementById("find-city");
+const defaultCity = "Madrid";
 
-let btn = d.getElementById('btn-find');
-let city = d.getElementById('find-city');
-let defaultCity = 'Madrid'
+d.addEventListener("DOMContentLoaded", () => {
+  let app = new App();
+  app.getFetchData(getlocalStorage());
+});
+ 
 
 
-d.addEventListener('DOMContentLoaded', () => {
-    if (localStorage.getItem('city') === null) {
-        city.value = defaultCity;
-      } else {
-        city.value = JSON.parse(localStorage.getItem("city"));
 
-      }
-
-      render(city.value);
-   
-})  
 
 btn.addEventListener("click", (e) => {
+  try{
+    let app = new App();
+  localStorage.setItem("city", JSON.stringify(city.value));
 
+  app.getFetchData(getlocalStorage());
+  }catch(error){
+    console.log(error);
+  }
+  
+  
 
-    localStorage.setItem("city", JSON.stringify(city.value));
-   
-
-    if (localStorage.getItem('city') === null) {
-        city.value = defaultCity;
-      } else {
-        city.value = JSON.parse(localStorage.getItem("city"));
-
-      }
-
-      render(city.value);
 
 });
 
 
-function render (city){
+class App {
+  constructor() {
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`)
-    .then((response) => response.json())
-    .then((weather) => {
-        ciudad.textContent = weather.name;
-        temp.textContent = Math.floor(weather.main.temp)+"°C";
-        time.textContent = new Date(weather.dt * 1000).toLocaleTimeString();
-        nubosidad.textContent = weather.clouds.all+"%";
-        humedad.textContent = weather.main.humidity+"%";
-        viento.textContent = Math.floor(weather.wind.speed*3.6)+"km/h";
-        atardecer.textContent = new Date(weather.sys.sunset * 1000).toLocaleTimeString();
-        psol.textContent = new Date (weather.sys.sunrise * 1000).toLocaleTimeString();
-        ltd.textContent = weather.sys.sunset;
-        desc.textContent = weather.weather[0].description;
-    })
-    .catch((error) => {
-        console.log(error);
-    })
 
+    this.ciudad = document.getElementById("name-city");
+    this.temp = document.getElementById("info-temp");
+    this.time = document.getElementById("info-time");
+    this.nubosidad = document.getElementById("info-nubosidad");
+    this.humedad = document.getElementById("info-humedad");
+    this.viento = document.getElementById("info-viento");
+    this.atardecer = document.getElementById("info-atardecer");
+    this.psol = document.getElementById("info-psol");
+    this.ltd = document.getElementById("info-ltd");
+    this.desc = document.getElementById("info-desc");
+    this._weather = {};
    
+  }
+
+
+
+   render(weather) { 
+    
+    this.ciudad.textContent = this._weather.name;
+    this.temp.textContent = Math.floor(this._weather.temp) + "°C";
+    this.time.textContent = new Date(this._weather.time * 1000).toLocaleTimeString();
+    this.nubosidad.textContent = this._weather.nubosidad + "%";
+    this.humedad.textContent = this._weather.humedad + "%";
+    this.viento.textContent = Math.floor(this._weather.viento * 3.6) + "km/h";
+    this.atardecer.textContent = new Date(
+      this._weather.atardecer * 1000
+    ).toLocaleTimeString();
+    this.psol.textContent = new Date(this._weather.psol * 1000).toLocaleTimeString();
+    this.ltd.textContent = this._weather.ltd;
+    this.desc.textContent = this._desc;
+  }
+
+
+  getFetchData(xCity) {
+    try {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${xCity}&appid=${apikey}&units=metric`
+      )
+        .then((response) => response.json())
+        .then((weather) => {
+          this._weather = {
+           name: weather.name,
+           temp: weather.main.temp,
+           time: weather.dt,
+           nubosidad: weather.clouds.all,
+           humedad: weather.main.humidity,
+           viento: weather.wind.speed,
+           atardecer: weather.sys.sunset,
+           psol: weather.sys.sunrise,
+           ltd: weather.sys.sunset,
+           desc: weather.weather[0].description
+          }
+  
+          this.render(this._weather);
+          
+        
+      
+        
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+  
+    } catch (error) {
+      console.log(error);
+    }
+    
+      
+      
+  }
+
+
+  
+
+}
+
+
+
+function getlocalStorage() {
+  try {
+    if (localStorage.getItem("city") === null) {
+      city.value = defaultCity;
+    } else {
+      city.value = JSON.parse(localStorage.getItem("city"));
+    }
+  
+    return city.value;  
+  } catch (error) {
+    console.log(error);
+  }
+  
 }
 
 
